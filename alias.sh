10cc2a4fe6248a02d5b364174703947b63be0025
Blessing Philips <blessingadesina@gmail.com>
2019-04-08 00:01:54 +0100

    my alias

---
+++ alias.sh
@@ -0,0 +1,22 @@
+#Directory
+alias showclix="cd /showclix"
+
+
+# Git
+alias gnedit="git commit --amend --no-edit"
+alias ga="git add"
+alias gc="git commit"
+alias gd="git diff"
+alias grc="git rebase --continue"
+alias gra="git rebase --abort"
+alias grs="git rebase --skip"
+alias gb="git branch -a"
+alias gch="git checkout"
+alias gp+="git push"
+alias gst="git status"
+
+# Node
+alias ns="npm start"
+alias nt="npm run test"
+alias nb="npm run build"
+alias npmfix="rm -rf node_modules/ && rm -f package-lock.json && npm install"
